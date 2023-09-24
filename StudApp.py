86e5e65aa2f7b8f048fdce73a46fe0b169a0e2f2
junxian777 <145646375+junxian777@users.noreply.github.com>
2023-09-24 20:14:02 +0800

    Update and rename EmpApp.py to StudApp.py

--- StudApp.py
+++ StudApp.py
@@ -25,7 +25,7 @@ table = 'student'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('studentRegisterForm.html')
 
 
 @app.route("/about", methods=['POST'])
