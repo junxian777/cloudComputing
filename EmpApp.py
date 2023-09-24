0daaf65461411222c7600fa85ad7d8c4b621e81f
junxian777 <145646375+junxian777@users.noreply.github.com>
2023-09-24 16:30:44 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from pymysql import connections
 import os
 import boto3
+from werkzeug.utils import secure_filename  # Import secure_filename
 from config import *
 
 app = Flask(__name__)
@@ -15,21 +16,17 @@ db_conn = connections.Connection(
     user=customuser,
     password=custompass,
     db=customdb
-
 )
 output = {}
 table = 'student'
-
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
-
 @app.route("/about", methods=['POST'])
 def about():
     return render_template('www.tarc.edu.my')
-
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -47,40 +44,28 @@ def AddEmp():
     stud_cgpa = request.form['stud_cgpa']
     lec_email = request.form['lec_email']
     com_email = request.form['com_email']
-    
 
-
-    insert_sql = "INSERT INTO student VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s , %s ,%s, %s, %s)"
+    insert_sql = "INSERT INTO student VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if stud_image_file.filename == "":
         return "Please select a file"
 
     try:
-
-        cursor.execute(insert_sql, (stud_id, stud_name, stud_gender, stud_IC, stud_email, stud_HP, stud_currAddress, stud_homeAddress, stud_programme, stud_image_file ,stud_pwd, stud_cgpa, lec_email ,com_email))
+        cursor.execute(insert_sql, (stud_id, stud_name, stud_gender, stud_IC, stud_email, stud_HP, stud_currAddress, stud_homeAddress, stud_programme, stud_image_file.filename, stud_pwd, stud_cgpa, lec_email, com_email))
         db_conn.commit()
-        #emp_name = "" + first_name + " " + last_name
-        # Uplaod image file in S3 #
-        stud_image_file_name_in_s3 = "stud-id-" + str(stud_id) + "_pdf"
+
+        # Securely generate a unique filename for the resume
+        updated_resume_filename = secure_filename(stud_image_file.filename)
+
+        # Uplaod image file in S3
+        stud_image_file_name_in_s3 = "stud-id-" + str(stud_id) + "_pdf/" + updated_resume_filename
         s3 = boto3.resource('s3')
 
         try:
             print("Data inserted in MySQL RDS... uploading pdf to S3...")
             s3.Bucket(custombucket).put_object(Key=stud_image_file_name_in_s3, Body=stud_image_file, ContentType='application/pdf')
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                stud_image_file_name_in_s3)
-
+            object_url = f"https://{custombucket}.s3.amazonaws.com/{stud_image_file_name_in_s3}"
         except Exception as e:
             return str(e)
 
@@ -88,9 +73,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('appStudOutput.html', name=stud_name)
-        #return to xinyi
+    return render_template('appStudOutput.html', name=stud_name, object_url=object_url)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-
